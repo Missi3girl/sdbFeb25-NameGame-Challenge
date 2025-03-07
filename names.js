4522dd1b2d552d575be303cba126c754f4bef357
Missy Eleazer <angelfish814@gmail.com>
2025-03-06 20:34:28 -0500

    created name challenge repo and copied my failed code to it to figure out later

---
+++ names.js
@@ -0,0 +1,20 @@
+/* 
+    ? Challenge Take Home
+    * create a placeholder variable
+    * create a string of names of everyone in the session
+    * loop over the string of names
+    * pass every character into the placeholder variable APART from any vowels
+    * console log the placeholder variable
+*/
+
+let sdbFeb25 = ["Ronnie", "Jaise", "India", "Wizzard", "Joseph", "Nikiva", "Missy", "Robert", "Samantha", "Gabe"]
+
+for (i of sdbFeb25) {
+    console.log(i);
+}
+
+for (let i = 0; i < sdbFeb25.length; i++) {
+    console.log(i);
+}
+    
+// I tried to do this based on my understanding and it didn't work, nor make sense. I asked google and chatgpt, and I just don't get it... I need to know why these things are done in order to aptly be able to execute this in any situation with the correct commands for that situation....
